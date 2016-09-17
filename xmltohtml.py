03bde22bca9a481cf1f552ca7904b4d537f412c7
William Doyle A F <williamdoyleaf@gmail.com>
2016-09-18 01:20:43 +0530

    version changed

--- xmltohtml.py
+++ xmltohtml.py
@@ -14,7 +14,7 @@ python xmltohtml.py -i foo.xml -o foo.html
 __author__      = "William Doyle A F"
 __copyright__   = "Copyright 2016"
 __license__ 	= "GNU GPLv3 (http://www.gnu.de/documents/gpl-3.0.en.html)"
-__version__ 	= "1.3"
+__version__ 	= "1.4"
 
 import re
 import sys

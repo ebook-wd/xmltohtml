8a75cf86fa9f63e35f3e67d287986d49ab136243
William Doyle A F <williamdoyleaf@gmail.com>
2016-09-21 20:20:44 +0530

    Added line spacing check and qlockquote tweaks

--- xmltohtml.py
+++ xmltohtml.py
@@ -30,6 +30,7 @@ next_font = ''
 next_header = ''
 
 #==============================================================================
+LINE_SPACING = 19
 PARA_INDENT = [252,253,254,255]
 BLOCK_QUOTE = ['254','255'] 
 unwanted_font = ['1','2','15']
@@ -48,8 +49,13 @@ FOOTNOTES_FONT = ['']
 #==============================================================================
 BLOCK_LOWER = int(BLOCK_QUOTE[0])    
 l = len(BLOCK_QUOTE)-1 
-BLOCK_UPPER = int(BLOCK_QUOTE[l])          
-                                                       
+BLOCK_UPPER = int(BLOCK_QUOTE[l])   
+       
+def match(BLOCK_QUOTE,PARA_INDENT):
+    for i in BLOCK_QUOTE:
+        if int(i) in PARA_INDENT:
+            return True
+        return False                                                       
 def header_match(header):
     for n in HEADER:
         if header == n:
@@ -80,6 +86,8 @@ def index_check(page_no):
     for i in INDEX:
         if i == page_no:
             return 0
+def check_line_spacing(line_top, nextline_top)   :
+    return nextline_top - line_top
 def superscript(line,font, line_top, nextline_top ):
     # checks for super script and subscript
     text_line=''
@@ -106,8 +114,7 @@ def paragraph_check(indent_next):
         if  n == indent_next :
             para = 1    
     return para                
-def check_line_spacing(line_top, nextline_top)   :
-    return nextline_top - line_top            
+            
 def main():
     Input_File_Name  = ''
 	# Assigns the out put file name
@@ -152,7 +159,7 @@ def main():
                     }''')
     fo.write(''' p {
                     padding: 0cm 0cm 0cm 0cm;
-                    text-indent: 45px;
+                    text-indent: 20px;
                     text-align:justify;
                     }
                     ''')
@@ -188,7 +195,7 @@ def main():
         page_no = page_no +1
         quote = 0
         center = 0
-        
+        previous_line_top = 0
         if index_check(page_no) == 0:
             #..........Skips the index pages...............
             continue
@@ -201,7 +208,6 @@ def main():
         fo.write("<a name="+pg_tag+"></a>"+"\n")
         
         image = soup.find_all('image')
-        #print(image)
         for i in image:
             s = BeautifulSoup(str(i),"lxml") 
             img = s.find('image')
@@ -210,7 +216,6 @@ def main():
             fo.write('''<img src="'''+str(img_tag)+'''"/><br/>''')
             
         tex=soup.find_all('text')
-        
         if page_no == CENTER :  
             #....Centralises copy right page
             fo.write("<center>"+"\n")
@@ -266,8 +271,10 @@ def main():
             indent = int(tag.get('left'))
             font = tag.get('font') 
             header =tag.get('top')
-            line_top = int(tag.get('top'))
-            
+            line_top = int(tag.get('top'))  
+                    
+            #print(line_top)
+
             if header_match(header) == 0 or font_match(font) == 0:
                 continue         
                          
@@ -290,17 +297,29 @@ def main():
                         # ..Biginning of blockquote.....
                         #fo.write("..quote...A...")
                         text_line="<blockquote>"+line+ "\n"
+                        line_gap = check_line_spacing(line_top, nextline_top)  
+                        if line_gap >= 2*LINE_SPACING - 2  and line_gap <= 2*LINE_SPACING + 2 :
+                            text_line = text_line +"<br/>" +"<br/>" +"\n"     
                         fo.write(text_line)
                         quote = 1 
                     elif quote == 0 and indent_next < BLOCK_LOWER : 
                         #.......Bigining of paragraph..............
-                        #fo.write("..quote..B...")
-                        text_line=line+ "\n"
+                        #fo.write("..quote..B...")                        
+                        if match(BLOCK_QUOTE,PARA_INDENT) == True:
+                            fo.write("..quote..B1...")
+                            text_line = line + "\n" 
+                        else:
+                            fo.write("..quote..B2...")
+                            text_line=line+"</p>" + "\n" + "<p>" +"\n" 
                         fo.write(text_line) 
                     elif quote ==1 and  indent_next>= BLOCK_LOWER and indent_next <= BLOCK_UPPER: 
                         #......Lines within blockquote
-                        #fo.write("..quote..C...")
-                        text_line = line+ "\n"
+                        #fo.write("..quote..C...")             
+                        line_gap = check_line_spacing(line_top, nextline_top)                           
+                        if line_gap >= 2*LINE_SPACING - 2  and line_gap <= 2*LINE_SPACING + 2 :
+                            text_line = line +"<br/>" +"<br/>" +"\n"  
+                        else:
+                            text_line = line+ "\n"                        
                         fo.write(text_line)  
                     elif quote ==1 and  indent_next>= BLOCK_LOWER : 
                         #......Lines within blockquote
@@ -312,59 +331,98 @@ def main():
                         #fo.write("..quote.E..")
                         text_line = line +"\n" 
                         fo.write(text_line) 
-                    elif quote ==1 and indent_next < BLOCK_LOWER and PARA_INDENT  == BLOCK_QUOTE : 
-                        #....Check begining of Paragraph after a block quote when both blockquote and paragraph have same indent
+                    elif quote ==1 and indent_next < BLOCK_LOWER :                         
                         #fo.write("..quote.F..")
-                        text_line=  "</blockquote>"+ "</p>" + "\n" + "<p>" +"\n" + line +"\n"
-                        fo.write(text_line)
-                        quote = 0
-                    elif quote ==1 and indent_next < BLOCK_LOWER and PARA_INDENT  != BLOCK_QUOTE:  
-                        #....Check begining of Paragraph after a block quote when both blockquote and paragraph have different indent
-                        #fo.write("..quote.G..")
-                        text_line= line +"\n" + "</blockquote>"+ "</p>" + "\n" + "<p>" +"\n"
-                        fo.write(text_line)
-                        quote = 0
+                        if match(BLOCK_QUOTE,PARA_INDENT) == True:
+                            #....Check begining of Paragraph after a block quote when both blockquote and paragraph have same indent
+                            fo.write("..quote..F1...")
+                            text_line=  "</blockquote>"+ "</p>" + "\n" + "<p>" +"\n" + line +"\n"
+                        else:
+                            fo.write("..quote..F2...")
+                            #....Check begining of Paragraph after a block quote when both blockquote and paragraph have different indent
+                            text_line= line +"\n" + "</blockquote>"+ "</p>" + "\n" + "<p>" +"\n"
+                        fo.write(text_line)                         
+                        quote = 0                    
                     else:
-                        pass 
+                        #pass 
+                        #fo.write("..quote.H..")
+                        text_line="<blockquote>"+line+ "\n"
+                        fo.write(text_line)    
+                        quote = 1
                 elif quote == 1 and indent > BLOCK_UPPER:
                     # Citation after the blockquote
-                    if indent_next>= BLOCK_LOWER and indent_next <= BLOCK_UPPER: 
-                        #fo.write("..quote..H.")
-                        text_line = "<br/>"+line +"</blockquote>"+"</p>" + "\n" +"<p>" +"\n" 
-                        fo.write(text_line) 
-                        quote = 0
-                    elif indent_next <= BLOCK_LOWER   :
+                    if indent_next>= BLOCK_LOWER and indent_next <= BLOCK_UPPER and line_top ==  previous_line_top: 
                         #fo.write("..quote..I.")
+                        text_line = line  +"\n" 
+                        line_gap = check_line_spacing(line_top, nextline_top)  
+                        if line_gap >= 2*LINE_SPACING - 2  and line_gap <= 2*LINE_SPACING + 2 :
+                            text_line = text_line +"<br/>" +"<br/>" +"\n"
+                        fo.write(text_line) 
+                    elif indent_next>= BLOCK_LOWER  and line_top ==  previous_line_top: 
+                        #fo.write("..quote..J1.")
+                        text_line = line  +"\n" 
+                        fo.write(text_line) 
+                        #quote = 0
+                    elif indent_next>= BLOCK_LOWER and indent_next <= BLOCK_UPPER and line_top !=  previous_line_top: 
+                        #fo.write("..quote..J.")
                         text_line = "<br/>"+line +"</blockquote>"+"</p>" + "\n" +"<p>" +"\n" 
                         fo.write(text_line) 
                         quote = 0
+                    elif indent_next <= BLOCK_LOWER and (line_top - previous_line_top == 1 or line_top - previous_line_top == -1):
+                        #fo.write("..quote..K.")
+                        text_line = line +"\n" 
+                        fo.write(text_line) 
+                    elif indent_next <= BLOCK_LOWER :
+                        #fo.write("..quote..L.")
+                        if line_top ==  previous_line_top:                            
+                            text_line = line +"</blockquote>"+"</p>" + "\n" +"<p>" +"\n" 
+                        else:
+                            text_line = "<br/>"+line +"</blockquote>"+"</p>" + "\n" +"<p>" +"\n" 
+                        fo.write(text_line) 
+                        quote = 0
+                    elif indent_next <= BLOCK_LOWER and line_top ==  previous_line_top: #
+                        #fo.write("..quote..M.")
+                        text_line = line +"</blockquote>"+"</p>" + "\n" +"<p>" +"\n" 
+                        fo.write(text_line) 
+                        quote = 0
+                    elif nextline_top < line_top or nextline_top > line_top:
+                        text_line = superscript(line,font, line_top, nextline_top )  #checks for super script and subscript
+                        if text_line == 'NULL':                        
+                            pass
+                        else:
+                            fo.write(text_line + "\n")
                     else:
-                        #fo.write("..quote..J.")
-                        text_line ="<br/>"+ line.rstrip('\n') +"\n" 
+                        #fo.write("..quote..N.")
+                        #text_line ="<br/>"+ line.rstrip('\n') +"\n" 
+                        text_line ="<br/>"+ line +"\n" 
                         fo.write(text_line)                                    
                 else: 
                    
                    if quote == 1 and indent_next < BLOCK_LOWER : 
-                       #fo.write("..quote..K.")
+                       #fo.write("..quote..O.")
                        text_line= "</blockquote>"+"</p> " + "\n" +"<p>"+ line + "\n" 
                        fo.write(text_line)
                        quote = 0 
                    text_line = superscript(line,font, line_top, nextline_top )  #checks for super script and subscript
                    if text_line == 'NULL':                        
-                        pass
+                        text_line = line
                    else:
                         fo.write(text_line + "\n")
                         continue
-                   line = footnote(line,font) #checks for footnotes     
+                   line = footnote(line,font) #checks for footnotes    
                    line_gap = check_line_spacing(line_top, nextline_top)   
                    if line_gap >= 2*LINE_SPACING - 2  and    line_gap <= 2*LINE_SPACING + 2 :
-                        text_line = line +"<br/>" +"<br/>" +"\n" 
+                        text_line = line +"<br/>" +"<br/>" +"\n"                      
                    p = paragraph_check(indent_next)
                    if p == 1:
-                       text_line = line+"</p>"+"\n"+"<p>"                                                                          
-                   else:
-                       text_line = line+"\n"                        
+                       text_line = text_line +"</p>"+"\n"+"<p>"                                                                          
+                   else:                        
+                       text_line = text_line +"\n"                                        
                    fo.write(text_line)
+                if SUPER_FONT[0] != '':
+                    if int(font) != int(SUPER_FONT[0]): 
+                        #print ("line_top =",line_top," previous_line_top =", previous_line_top)
+                        previous_line_top = line_top   
         if center == 1:
             fo.write("</center>"+"\n")
             center = 0                                                                                    
@@ -387,4 +445,3 @@ def usage():
 
 if __name__ == '__main__':
     main()
-
